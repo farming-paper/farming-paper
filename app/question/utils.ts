d0033595ebd654bfd3e38070bbc3d12415a42352
Kim Tae Hoon <eszqsc112@gmail.com>
2022-12-02 09:14:49 +0900

    update questions

--- app/question/utils.ts
+++ app/question/utils.ts
@@ -2,11 +2,19 @@ import { deepclone } from "~/util";
 import type { MapKey } from "~/util-type";
 import { questions as adspQuestions } from "./data/adsp";
 import { questions as economicsQuestions } from "./data/economics";
+import { questions as mathematicalStatisticsQuestions } from "./data/mathematical-statistics";
 
 export function getQuestionGroups() {
   return new Map([
     ["adsp", { name: "ADSP", questions: deepclone(adspQuestions) }],
     ["economics", { name: "경제학", questions: deepclone(economicsQuestions) }],
+    [
+      "statistics",
+      {
+        name: "수리통계학",
+        questions: deepclone(mathematicalStatisticsQuestions),
+      },
+    ],
   ] as const);
 }
 
