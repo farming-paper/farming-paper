1f5297ed43625cd64808332972f5e6a6a53e38a7
Kim Tae Hoon <eszqsc112@gmail.com>
2023-01-06 17:05:38 +0900

    아래 nav 보완

--- app/common/components/BottomNav.tsx
+++ app/common/components/BottomNav.tsx
@@ -1,28 +1,44 @@
+import {
+  BulbOutlined,
+  FileAddOutlined,
+  OrderedListOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import { Link } from "@remix-run/react";
 
 export default function BottomNav() {
   return (
     <section
       id="bottom-navigation"
-      className="fixed inset-x-0 bottom-0 left-0 right-0 z-10 block max-w-md mx-auto bg-white shadow"
+      className="fixed inset-x-0 bottom-0 left-0 right-0 z-10 block max-w-md mx-auto border-t shadow-lg bg-white/50 backdrop-blur"
     >
       <div id="tabs" className="flex justify-between">
+        <Link
+          to={"/q/solve"}
+          className="justify-center inline-block w-full pt-2 pb-1 text-center focus:text-teal-500 hover:text-teal-500"
+        >
+          <BulbOutlined />
+          <span className="block text-xs tab tab-explore">Solve</span>
+        </Link>
         <Link
           to={"/q/list"}
           className="justify-center inline-block w-full pt-2 pb-1 text-center focus:text-teal-500 hover:text-teal-500"
         >
+          <OrderedListOutlined />
           <span className="block text-xs tab tab-explore">List</span>
         </Link>
         <Link
           to={"/q/new"}
           className="justify-center inline-block w-full pt-2 pb-1 text-center focus:text-teal-500 hover:text-teal-500"
         >
+          <FileAddOutlined />
           <span className="block text-xs tab tab-whishlist">New</span>
         </Link>
         <Link
           to={"/account"}
           className="justify-center inline-block w-full pt-2 pb-1 text-center focus:text-teal-500 hover:text-teal-500"
         >
+          <UserOutlined />
           <span className="block text-xs tab tab-account">Account</span>
         </Link>
       </div>
--- app/routes/__authorized/q/list/index.tsx
+++ app/routes/__authorized/q/list/index.tsx
@@ -1,9 +1,118 @@
-import { redirect } from "@remix-run/server-runtime";
+import { CalendarIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import { Link, useLoaderData, useNavigate, useParams } from "@remix-run/react";
+import type { LoaderArgs } from "@remix-run/server-runtime";
+import { json, redirect } from "@remix-run/server-runtime";
+import { Pagination } from "antd";
+import dayjs from "dayjs";
+import type { PartialDeep } from "type-fest";
+import { getSessionWithProfile } from "~/auth/get-session";
+import { createQuestion } from "~/question/create";
+import type { Question } from "~/question/types";
+import { getServerSideSupabaseClient } from "~/supabase/client";
 
-export async function loader() {
-  return redirect("/q/list/1");
+const numberPerPage = 10;
+
+export async function loader({ request }: LoaderArgs) {
+  const url = new URL(request.url);
+  const pageStr = url.searchParams.get("p");
+  if (!pageStr) {
+    url.searchParams.set("p", "1");
+    return redirect(url.pathname + url.search);
+  }
+
+  const response = new Response();
+  const page = Number.parseInt(pageStr) || 1;
+  const { profile } = await getSessionWithProfile({ request, response });
+  const db = getServerSideSupabaseClient();
+
+  const questionsRes = await db
+    .from("questions")
+    .select("*", { count: "exact" })
+    .eq("creator", profile.id)
+    .order("updated_at", { ascending: false })
+    .range((page - 1) * numberPerPage, page * numberPerPage - 1);
+
+  if (questionsRes.error) {
+    throw new Error(questionsRes.error.message);
+  }
+
+  if (questionsRes.count === null) {
+    throw new Error("questionsRes.count is null");
+  }
+
+  return json({
+    items: questionsRes.data.map((q) => ({
+      ...q,
+      content: createQuestion(q.content as PartialDeep<Question>),
+    })),
+    total: questionsRes.count,
+  });
 }
 
-export default function List() {
-  return <></>;
+export default function QuestionList() {
+  const loaded = useLoaderData<typeof loader>();
+  const params = useParams();
+  const page = Number.parseInt(params.page || "1");
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col">
+      <header className="flex items-end gap-4 mx-5 my-5">
+        <h1 className="m-0 text-xl font-medium leading-none">문제 리스트</h1>
+        <p className="m-0 leading-none text-gray-400">{page}p</p>
+      </header>
+      <ul className="divide-y divide-gray-200">
+        {loaded.items.map((item) => (
+          <li key={item.public_id}>
+            <Link
+              className="block hover:bg-gray-50"
+              to={`/q/edit/${item.public_id}`}
+            >
+              <div className="flex items-center px-4 py-4 sm:px-6">
+                <div className="flex-1 min-w-0 sm:flex sm:items-center sm:justify-between">
+                  <div className="truncate">
+                    <div className="flex text-sm">
+                      <p className="m-0 font-medium truncate">
+                        {item.content.message}
+                      </p>
+                    </div>
+                    <div className="flex mt-2">
+                      <div className="flex items-center text-sm text-gray-500">
+                        <CalendarIcon
+                          className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                          aria-hidden="true"
+                        />
+                        <span>
+                          {dayjs(item.updated_at).format("YYYY-MM-DD HH:mm:ss")}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex-shrink-0 ml-5">
+                  <ChevronRightIcon
+                    className="w-5 h-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </div>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className="flex flex-col items-center justify-center my-4">
+        <Pagination
+          defaultCurrent={page}
+          pageSize={numberPerPage}
+          total={loaded.total}
+          onChange={(e) => {
+            const dest = new URL(document.location.href);
+            dest.searchParams.set("p", e.toString());
+            navigate(`${dest.pathname}${dest.search}`);
+          }}
+        />
+      </div>
+    </div>
+  );
 }
