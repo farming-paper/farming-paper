02662696289aed0a63e0ab58c80e39992983a517
Kim Tae Hoon <eszqsc112@gmail.com>
2023-01-28 16:20:40 +0900

    await logouts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,9 @@
 import type { LoaderArgs } from "@remix-run/server-runtime";
 import { redirect } from "@remix-run/server-runtime";
+import { createServerClient } from "@supabase/auth-helpers-remix";
 import { getSessionWithProfile } from "~/auth/get-session";
+import { getServerSideSupabaseConfig } from "~/config";
+import type { Database } from "~/supabase/generated/supabase-types";
 
 export const loader = async ({ request }: LoaderArgs) => {
   const response = new Response();
@@ -10,16 +13,23 @@ export const loader = async ({ request }: LoaderArgs) => {
       response,
     });
     if (!session || !profile) {
-      supabaseClient.auth.signOut();
+      await supabaseClient.auth.signOut();
       return redirect("/login", {
         headers: response.headers,
       });
     }
   } catch (e) {
+    const { serviceRoleKey, url } = getServerSideSupabaseConfig();
+    const supabaseClient = createServerClient<Database>(url, serviceRoleKey, {
+      request,
+      response,
+    });
+
+    await supabaseClient.auth.signOut();
     return redirect("/login", {
       headers: response.headers,
     });
   }
 
-  return redirect("/q/solve");
+  return redirect("/q/solve?status=entry");
 };
