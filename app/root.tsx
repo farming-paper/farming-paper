c5a912e2ce4c09a24d98c030f45d2ac17eeed48f
Kim Tae Hoon <eszqsc112@gmail.com>
2023-01-29 16:00:59 +0900

    revert viewport

--- app/root.tsx
+++ app/root.tsx
@@ -44,7 +44,7 @@ export function links() {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Farming Paper",
-  viewport: "width=device-width,user-scalable=no",
+  viewport: "width=device-width,initial-scale=1",
 });
 
 export const loader = async ({ request }: LoaderArgs) => {
