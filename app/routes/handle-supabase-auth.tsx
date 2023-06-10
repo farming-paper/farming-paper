b843cc166df1940c682df72a876b6ddd0629d0e2
Kim Tae Hoon <eszqsc112@gmail.com>
2023-06-10 13:53:21 +0900

    add eslint disable comment

--- app/routes/handle-supabase-auth.tsx
+++ app/routes/handle-supabase-auth.tsx
@@ -1,6 +1,7 @@
 // this is used to tell Remix to call active loaders
 // after a user signs in or out
 export const action = () => {
+  // eslint-disable-next-line no-console
   console.log("handle supabase auth");
   return null;
 };
