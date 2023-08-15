c3c86a12fdada635d86a451ad253c398a923a391
Kim Tae Hoon <eszqsc112@gmail.com>
2023-08-15 12:59:45 +0900

    remove vercel and add prisma generate on build

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   publicPath: "/build/",
-  serverBuildPath: "api/index.js",
   serverMainFields: ["main", "module"],
   serverModuleFormat: "cjs",
   serverPlatform: "node",
@@ -9,7 +8,6 @@ module.exports = {
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
   serverDependenciesToBundle: ["nanoid", "marked"],
   future: {
