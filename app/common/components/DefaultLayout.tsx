b04413fbdd28164f747153252ca4a6357024697d
Kim Tae Hoon <eszqsc112@gmail.com>
2023-12-28 20:53:51 +0900

    add DefaultLayout

---
+++ app/common/components/DefaultLayout.tsx
@@ -0,0 +1,30 @@
+import type { ReactNode } from "react";
+
+export default function DefaultLayout({
+  children,
+  footer,
+  header,
+  sidebarBottom,
+  sidebarTop,
+}: {
+  children: ReactNode;
+  sidebarTop?: ReactNode;
+  sidebarBottom?: ReactNode;
+  header?: ReactNode;
+  footer?: ReactNode;
+}) {
+  return (
+    <div className="flex">
+      <div className="flex flex-col justify-between w-12 h-screen text-gray-300 bg-gray-700">
+        <div>{sidebarTop}</div>
+        <div>{sidebarBottom}</div>
+      </div>
+
+      <div className="flex flex-col flex-1 min-h-screen">
+        {header}
+        {children}
+        {footer}
+      </div>
+    </div>
+  );
+}
--- app/routes/_auth.dashboard.tsx
+++ app/routes/_auth.dashboard.tsx
@@ -1,15 +1,18 @@
 import { Link, useOutletContext } from "react-router-dom";
+import DefaultLayout from "~/common/components/DefaultLayout";
 import type { IOutletProps } from "~/types";
 
 export default function Dashboard() {
   const context = useOutletContext<IOutletProps>();
 
   return (
-    <div>
-      <h1>Welcome to the Auth Dashboard!</h1>
-      <pre>{context.session?.user.email}</pre>
-      <p>User:</p>
-      <Link to="/logout">Logout</Link>
-    </div>
+    <DefaultLayout>
+      <div>
+        <h1>Welcome to the Auth Dashboard!</h1>
+        <pre>{context.session?.user.email}</pre>
+        <p>User:</p>
+        <Link to="/logout">Logout</Link>
+      </div>
+    </DefaultLayout>
   );
 }
