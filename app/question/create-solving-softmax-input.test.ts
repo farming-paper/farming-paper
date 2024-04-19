8be19e6f2055f3c24423e538ab30a87c67b27ecd
Kim Tae Hoon <eszqsc112@gmail.com>
2024-04-19 09:19:39 +0900

    fix test softmax #103

--- app/question/create-solving-softmax-input.test.ts
+++ app/question/create-solving-softmax-input.test.ts
@@ -11,17 +11,17 @@ test("금방 전에 한 번 맞추면 0.25", () => {
   expect(result).toBeCloseTo(0.25887, 5);
 });
 
-test("3일간 세 번 맞았으면 0", () => {
+test("3일간 세 번 맞았으면 -0.37", () => {
   const data = [
     { elapsed_min: 1440, value: 1 },
     { elapsed_min: 2880, value: 1 },
     { elapsed_min: 4320, value: 1 },
   ];
   const result = createSoftmaxInputV1(data);
-  expect(result).toBeCloseTo(0, 5);
+  expect(result).toBeCloseTo(-0.36708458373, 5);
 });
 
-test("3일간 세 번 틀렸으면 0.4 언저리", () => {
+test("3일간 세 번 틀렸으면 0.4", () => {
   const data = [
     { elapsed_min: 1440, value: 0 },
     { elapsed_min: 2880, value: 0 },
@@ -31,7 +31,7 @@ test("3일간 세 번 틀렸으면 0.4 언저리", () => {
   expect(result).toBeCloseTo(0.37573, 5);
 });
 
-test("하루간 세 번 틀렸으면 0.15 언저리", () => {
+test("하루간 세 번 틀렸으면 0.15", () => {
   const data = [
     { elapsed_min: 300, value: 0 },
     { elapsed_min: 600, value: 0 },
@@ -41,27 +41,27 @@ test("하루간 세 번 틀렸으면 0.15 언저리", () => {
   expect(result).toBeCloseTo(0.14568, 5);
 });
 
-test("하루간 세 번 맞았으면 0", () => {
+test("하루간 세 번 맞았으면 -0.6", () => {
   const data = [
     { elapsed_min: 300, value: 1 },
     { elapsed_min: 600, value: 1 },
     { elapsed_min: 900, value: 1 },
   ];
   const result = createSoftmaxInputV1(data);
-  expect(result).toBeCloseTo(0, 5);
+  expect(result).toBeCloseTo(-0.599261600551, 5);
 });
 
-test("일주일 전에 세 번 맞았으면 0", () => {
+test("일주일 전에 세 번 맞았으면 -0.2", () => {
   const data = [
     { elapsed_min: 10000, value: 1 },
     { elapsed_min: 10000, value: 1 },
     { elapsed_min: 10000, value: 1 },
   ];
   const result = createSoftmaxInputV1(data);
-  expect(result).toBeCloseTo(0, 5);
+  expect(result).toBeCloseTo(-0.1943215116604, 5);
 });
 
-test("일주일 전에 세 번 틀렸으면 0.52453", () => {
+test("일주일 전에 세 번 틀렸으면 0.5", () => {
   const data = [
     { elapsed_min: 10000, value: 0 },
     { elapsed_min: 10000, value: 0 },
